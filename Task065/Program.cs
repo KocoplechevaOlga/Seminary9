c0d37ac7ed97fc9581b51ca545e46eb0caa18fcf
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-28 15:37:06 +0300

    Task 65

--- Task065/Program.cs
+++ Task065/Program.cs
@@ -1,4 +1,16 @@
 ﻿// Задайте значения M и N. Напишите программу, которая
 //выведет все натуральные числа в промежутке от M до N.
 
+void NaturelNumbers(int min, int max)
+{
+    if (max == min) return;
+    NaturelNumbers(min, max - 1);
+    Console.Write($"{max} ");
+}
 
+Console.WriteLine("Введите начало отчета:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конец отчета:");
+int  m = Convert.ToInt32(Console.ReadLine());
+
+NaturelNumbers(n, m);
