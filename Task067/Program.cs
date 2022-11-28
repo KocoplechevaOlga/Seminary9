e921d9b904df3f1db7f0a3ce98e138663b41e948
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-28 15:55:28 +0300

    Task 67

--- Task067/Program.cs
+++ Task067/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Напишите программу, которая будет принимать на вход число 
 //и возвращать сумму его цифр.
 
+int SumByDig(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumByDig(num /10);
+}
 
+Console.WriteLine("Введите натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int s = SumByDig(n);
+Console.WriteLine($"Сумма цифр числа {n} составляет {s}");
