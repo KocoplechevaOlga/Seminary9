321387ef701002835f8efe6f77a34eec15badb51
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-11-28 14:59:43 +0300

    Task 63

--- Task063/Program.cs
+++ Task063/Program.cs
@@ -1,4 +1,14 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет
 //все натуральные числа в промежутке от 1 до N.
 
+void NaturelNumbers(int num)
+{
+    if (num == 0) return;
+    NaturelNumbers(num - 1);
+    Console.Write($"{num}, ");
+}
+
+Console.WriteLine("Введите натуральное число");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturelNumbers(n);
 
